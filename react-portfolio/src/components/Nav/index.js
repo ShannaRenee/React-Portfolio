7cb002771489d510daabf1d52ff6ceac892a2d11
Calcifer <shanna.renee.morris@gmail.com>
2023-08-25 14:03:39 -0700

    added styling to the header

--- react-portfolio/src/components/Nav/index.js
+++ react-portfolio/src/components/Nav/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 const Nav = ({pages, currentPage, setCurrentPage}) => {
   return (
-    <div>Nav</div>
+    <nav className='nav d-flex justify-content-between-align-items-center'>
+        {pages.map(page => (
+            <a 
+            href='/#' 
+            key={page} 
+            className={`nav-link ${page === currentPage && 'active'}`}
+            onClick={() => setCurrentPage(page)}
+            >
+                {page}
+            </a>
+        ))}
+    </nav>
   )
 }
 
