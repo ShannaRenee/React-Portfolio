6d2b47d263803796f7455171fdde2a81a00adf10
Calcifer <shanna.renee.morris@gmail.com>
2023-08-30 21:25:24 -0700

    added style to cards

--- react-portfolio/src/components/Project/index.js
+++ react-portfolio/src/components/Project/index.js
@@ -1,8 +1,28 @@
 import React from 'react'
 
-const Project = () => {
+const Project = ({project}) => {
   return (
-    <div>Project</div>
+    <div className='col'>
+        <div class='card'>
+            <img src={require(`../../assets/projects/${project.name}`)} 
+            class='card-img' 
+            alt={project.name}
+            />
+            <div class='card-img-overlay'>
+                <h3 class='card-title'>
+                    <a href={project.link} className='card-text'>
+                        {project.name}
+                    </a>
+                    <a href={project.repo} className='card-text'>
+                        <i className='fab fa-github'></i>
+                    </a>
+                </h3>
+                <p class='card-text'>
+                    {project.description}
+                </p>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- react-portfolio/src/page/Portfolio.js
+++ react-portfolio/src/page/Portfolio.js
@@ -4,7 +4,7 @@ import Project from '../components/Project'
 
 const Portfolio = () => {
   return (
-    <div>
+    <div className='row row-cols-1-row-cols-md-2 row-cols-xl-3 g-4'>
       {projects.map((project, i) => (
         <Project project={project}
         key={i} />
