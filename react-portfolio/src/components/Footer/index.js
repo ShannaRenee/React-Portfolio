5f591965c46ef5d3e5f4f67046af1f8117be5f75
Calcifer <shanna.renee.morris@gmail.com>
2023-08-31 00:50:10 -0700

    setting up footer

--- react-portfolio/src/components/Footer/index.js
+++ react-portfolio/src/components/Footer/index.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import './style.css';
 
 const Footer = () => {
+  const icons = [
+    {
+      name: 'fab fa-github',
+      link: 'https://github.com'
+    },
+    {
+      name: 'fab fa-linkedin',
+      link: 'https://linkedin.com'
+    },
+    {
+      name: 'fab fa-stack-overflow',
+      link: 'https://stackoverflow.com'
+    },
+  ]
   return (
-    <div>Footer</div>
+    <footer className='d-flex justify-content-center align-items-center'>
+      {icons.map((icon) => (
+        <a href={icon.link} key={icon.name} target='_blank'
+        rel='noreferrer' className='mx-3'>
+          <i className={icon.name}></i>
+          </a>
+      ))}
+    </footer>
   )
 }
 
