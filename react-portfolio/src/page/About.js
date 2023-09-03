2823780f3659185a238d8cc301e9b97bd0988757
Calcifer <shanna.renee.morris@gmail.com>
2023-09-03 15:23:35 -0700

    added info to about page

--- react-portfolio/src/page/About.js
+++ react-portfolio/src/page/About.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import pic from '../assets/profesh_pic/Profesh_pic.jpeg'
 
 const About = () => {
   return (
     <>
     <div className='d-flex align-items-center'>
-    <i className='fas fa-user-circle me-4'></i>
-    <p>sdfbsdfbsdfbsdb</p>
+    <img src={pic} alt='pic of me'/>
+    <p>Hey there! My name is Shanna, and I am a full-stack web developer based in Tucson, AZ. 
+      I really enjoy challenging myself to learn new technologies so that I can further my coding knowledge
+      and expand my skillset. While I have experience working with both back-end and front-end technologies, 
+      my passion lies in translating the vision I have in my mind onto the screen. Playing around in CSS files
+      is one of my new favorite hobbies. Check out my portfolio and send me a message through my contact page if
+      you have any questions!
+    </p>
     </div>
     </>
   )
